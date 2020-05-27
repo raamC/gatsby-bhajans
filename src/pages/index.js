a6a2ef9625be93b6b27e8a6b56cb480fb623e4e7
raamC <raamrajchauhan@gmail.com>
2020-05-27 21:55:21 +0100

    Add Bulma UI

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,8 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import "../styles/index.scss"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
@@ -12,6 +14,19 @@ const IndexPage = () => (
       <li><Link to="/hanuman-chalisa/">Hanuman Chalisa</Link></li>
       
     </ul>
+
+    <div className="container">
+      <div className="columns">
+        <div className="column">
+          <h2 className="title is-2">Level 2 heading</h2>
+          <p className="content">Cool content. Using Bulma!</p>
+        </div>
+        <div className="column is-four-fifths">
+          <h2 className="title is-2">Level 2 heading</h2>
+          <p className="content">This column is cool too!</p>
+        </div>
+      </div>
+    </div>
 
 
   </Layout>
