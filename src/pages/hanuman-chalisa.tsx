713fff3c4cf02af0aa7a9124af50c78e1a710158
raamC <raamrajchauhan@gmail.com>
2020-05-27 20:19:19 +0100

    Proof of principle with JSON input

---
+++ src/pages/hanuman-chalisa.tsx
@@ -0,0 +1,30 @@
+// Gatsby supports TypeScript natively!
+import React from "react"
+import { PageProps, Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+import JSONData from "../content/hanumanChalisa.json"
+
+const SecondPage = (props: PageProps) => (
+  <Layout>
+    <SEO title="Hanuman Chalisa" />
+    <h1>Hanuman Chalisa</h1>
+
+
+    <div style={{ maxWidth: `960px`, margin: `1.45rem` }}>
+  { JSONData.verse1.gujarati }
+  </div>
+  <div style={{ maxWidth: `960px`, margin: `1.45rem` }}>
+  { JSONData.verse1.transliteration }
+  </div>
+  <div style={{ maxWidth: `960px`, margin: `1.45rem` }}>
+  { JSONData.verse1.translation }
+
+</div>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default SecondPage
