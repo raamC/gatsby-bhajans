e10a17bc6d82f08c881b3436654ae7af5633895e
raamC <raamrajchauhan@gmail.com>
2020-05-28 21:44:56 +0100

    Temporarily remove icon from config

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         background_color: `#eb9234`,
         theme_color: `#eb9234`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
