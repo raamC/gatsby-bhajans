976ee742dc18df8ca698d026d89391746e9aff42
raamC <raamrajchauhan@gmail.com>
2020-05-28 21:42:37 +0100

    Update homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,60 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-
-import "../styles/index.scss"
 
 const IndexPage = () => (
   <Layout>
-    
-    <h1>List of bhajans</h1>
-    <ul>
-      <li><Link to="/hanuman-chalisa/">Hanuman Chalisa</Link></li>
-      
-    </ul>
-
-    <div className="container">
-      <div className="columns">
-        <div className="column">
-          <h2 className="title is-2">Level 2 heading</h2>
-          <p className="content">Cool content. Using Bulma!</p>
-        </div>
-        <div className="column is-four-fifths">
-          <h2 className="title is-2">Level 2 heading</h2>
-          <p className="content">This column is cool too!</p>
+    <section class="hero is-medium is-info">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title is-1">The Bhajan Project</h1>
+          <h2 class="subtitle">A shared effort to explain and preserve our bhajans</h2>
         </div>
       </div>
-    </div>
+    </section>
+
+
+    <section class="section">
+      <div class="container">
+        <div class="tile is-ancestor">
+          <div class="tile is-parent">
+            <article class="tile is-child box">
+              <h1 class="title has-text-centered is-4">Collect</h1>
+              <p class="has-text-centered">
+                We hope to gather together the bhajans and make them easily available in one place 
+              </p>
+            </article>
+          </div>
+          <div class="tile is-parent">
+            <article class="tile is-child box">
+            <span class="fas fa-camera"></span>
+            <h1 class="title has-text-centered is-4">Preserve</h1>
+            <p class="has-text-centered">
+                We want to digitise the bhajans and make sure they are in an accessible format
+              </p>
+              
+                </article>
+          </div>
+          <div class="tile is-parent">
+            <article class="tile is-child box">
+            <h1 class="title has-text-centered is-4">Translate</h1>
+            <p class="has-text-centered">We aim to translate the bhajans to share the meanings with future generations</p>
+          </article>
+          </div>
+        </div>
+
+
+      </div>
+    </section>
+
+
+
+    <section class="section">
+      <div class="container">
+        <h1 class="title">We are working together to collect and translate bhajans for the future generations</h1>
+      </div>
+    </section>
+
 
 
   </Layout>
