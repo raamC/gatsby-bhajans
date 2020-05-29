60f03086e569bd2cd3dea9e5168febd2da3555a9
raamC <raamrajchauhan@gmail.com>
2020-05-29 21:35:08 +0100

    Format hanuman chalisa page

--- src/pages/hanuman-chalisa.js
+++ src/pages/hanuman-chalisa.js
@@ -1,21 +1,50 @@
-import React from "react"
+import React, { useState } from "react"
 import Layout from "../components/layout"
 import JSONData from "../content/hanumanChalisa.json"
 
-const HanumanChalisaPage = () => (
-  <Layout>
+const HanumanChalisaPage = () => {
+  const [showGujarati, setShowGujarati] = useState(true)
+  const [showTranslation, setShowTranslation] = useState(true)
 
-<div style={{ maxWidth: `960px`, margin: `1.45rem` }}>
-  { JSONData.verse1.gujarati }
-  </div>
-  <div style={{ maxWidth: `960px`, margin: `1.45rem` }}>
-  { JSONData.verse1.transliteration }
-  </div>
-  <div style={{ maxWidth: `960px`, margin: `1.45rem` }}>
-  { JSONData.verse1.translation }
-  </div>
+  return (
+    <Layout>
+      <section class="section">
+        <div class="container">
+          <div class="columns" style={{ display: 'flex', 'flex-wrap': 'wrap-reverse' }}>
 
-  </Layout>
-)
+            <div class="column is-two-thirds" style={{'min-width': '60%'}}>
+              <div class="notification">
+                <h1 class="title is-1">Hanuman Chalisa</h1>
+
+                <div style={showGujarati ? {} : { display: 'none' }} >
+                  {JSONData.verse1.gujarati}
+                </div>
+                <div>
+                  {JSONData.verse1.transliteration}
+                </div>
+                <div style={showTranslation ? {} : { display: 'none' }}>
+                  {JSONData.verse1.translation}
+                </div>
+              </div>
+
+            </div>
+            <div class="column is-one-third">
+              <div class="notification">
+                    <h3 class="title">Options</h3>
+                    <button class="button" style={{margin: '0 .5rem .5rem 0'}} onClick={() => setShowGujarati(!showGujarati)}>
+                      {showGujarati ? "Hide" : "Show"} Gujarati
+                    </button>
+                    <button class="button" style={{margin: '0 .5rem .5rem 0'}} onClick={() => setShowTranslation(!showTranslation)}>
+                      {showTranslation ? "Hide" : "Show"} translation
+                    </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+    </Layout>
+  )
+}
 
 export default HanumanChalisaPage
