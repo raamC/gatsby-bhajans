0e0ecd82a74a3819c489d1db862380cf9ffeb07e
raamC <raamrajchauhan@gmail.com>
2020-05-29 21:01:40 +0100

    Add font awesome and update homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
+import StatefulButton from "../components/statefulButton"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCopy, faSave, faExchangeAlt} from '@fortawesome/free-solid-svg-icons'
 
 const IndexPage = () => (
   <Layout>
     <section class="hero is-medium is-info">
       <div class="hero-body">
         <div class="container">
-          <h1 class="title is-1">The Bhajan Project</h1>
+          <h1 class="title is-1">The Bhajan Collection</h1>
           <h2 class="subtitle">A shared effort to explain and preserve our bhajans</h2>
         </div>
       </div>
@@ -19,15 +22,31 @@ const IndexPage = () => (
         <div class="tile is-ancestor">
           <div class="tile is-parent">
             <article class="tile is-child box">
-              <h1 class="title has-text-centered is-4">Collect</h1>
+
+              <h1 class="title has-text-centered is-4">
+              <FontAwesomeIcon className=" fa-3x" icon={faCopy}/>
+                </h1>
+                <h1 class="title has-text-centered is-4">
+                Collect</h1>
               <p class="has-text-centered">
-                We hope to gather together the bhajans and make them easily available in one place 
+                We hope to gather together bhajans and make them easily available in one place 
               </p>
             </article>
           </div>
           <div class="tile is-parent">
             <article class="tile is-child box">
-            <span class="fas fa-camera"></span>
+            <h1 class="title has-text-centered is-4">
+              <FontAwesomeIcon className=" fa-3x" icon={faExchangeAlt}/>
+                </h1>
+            <h1 class="title has-text-centered is-4">Translate</h1>
+            <p class="has-text-centered">We aim to translate the bhajans to share the meanings with future generations</p>
+          </article>
+          </div>
+          <div class="tile is-parent">
+            <article class="tile is-child box">
+            <h1 class="title has-text-centered is-4">
+              <FontAwesomeIcon className=" fa-3x" icon={faSave}/>
+                </h1>
             <h1 class="title has-text-centered is-4">Preserve</h1>
             <p class="has-text-centered">
                 We want to digitise the bhajans and make sure they are in an accessible format
@@ -35,27 +54,16 @@ const IndexPage = () => (
               
                 </article>
           </div>
-          <div class="tile is-parent">
-            <article class="tile is-child box">
-            <h1 class="title has-text-centered is-4">Translate</h1>
-            <p class="has-text-centered">We aim to translate the bhajans to share the meanings with future generations</p>
-          </article>
-          </div>
+        
         </div>
-
-
       </div>
     </section>
-
-
 
     <section class="section">
       <div class="container">
         <h1 class="title">We are working together to collect and translate bhajans for the future generations</h1>
       </div>
     </section>
-
-
 
   </Layout>
 )
