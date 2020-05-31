63721b77acff1f6eed7c78369b67f679e467696a
raamC <raamrajchauhan@gmail.com>
2020-05-31 13:59:25 +0100

    Update header

--- src/components/header.js
+++ src/components/header.js
@@ -3,21 +3,21 @@ import { Link } from "gatsby"
 import "../styles/index.scss"
 
 const Header = () => (
-  <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+  <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
       <div class="navbar-item">
-        <Link to="/">
-          The Bhajan Collection
-      </Link>
+        <Link to="/">Home</Link>
       </div>
-
     </div>
 
-    <div class="navbar-end">
-      <div class="navbar-item">
-        <Link to="/bhajans">
-          Bhajans
-      </Link>
+    <div id="navMenu" class="navbar-menu">
+      <div class="navbar-start">
+        <div class="navbar-item">
+          <Link to="bhajans" >Bhajans</Link>
+        </div>
+        <div class="navbar-item">
+          <Link to="mantras" >Mantras</Link>
+        </div>
       </div>
     </div>
   </nav>
